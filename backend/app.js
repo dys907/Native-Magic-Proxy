7e1222c6b32aa207a7286565c669cb7b2283f3b1
Dylan Sung <dys907@gmail.com>
2025-01-15 00:46:06 -0800

    separated app.js into scaffold

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
-const express = require("express");
-const fs = require("fs").promises;
-const path = require("path");
+// const express = require("express");
+// const fs = require("fs").promises;
+// const path = require("path");
+// const dotenv = require("dotenv");
+
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+
+//routers
+const buildDeckRouter = require("./src/routes/buildDeckRouter");
+
+
+//use routers
+
+//middleware
+app.use(express.text())
+
+app.use("/buildDeck", buildDeckRouter);
 
 app.get("/", (req, res) => {
   res.send(
@@ -11,57 +30,13 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/readfile", async (req, res) => {
-  const localstore = [];
-  let processedDeck = [];
-  const filePath = path.join(__dirname, "samplemox.txt");
-  try {
-    const data = await fs.readFile(filePath, "utf8");
-    localstore.push(data);
-    const lines = localstore[0].split("\n");
-    const deck = lines
-      .map((line) => {
-        const match = line.match(/^(\d+)\s+([^()]+)\s+\([^()]+\)/);
-        if (match) {
-          const quantity = parseInt(match[1], 10);
-          const name = match[2].trim();
-          return { quantity, name };
-        }
-        return null;
-      })
-      .filter((entry) => entry !== null);
-    processedDeck = deck;
-  } catch (err) {
-    res.status(500).send("Error reading file");
-    return;
-  }
-
-  const dataPath = path.join(
-    __dirname,
-    "data",
-    "default-cards-20250112224609.json"
-  );
-  try {
-    const data = await fs.readFile(dataPath, "utf8");
-    const cardData = JSON.parse(data);
-    const cardInfo = processedDeck.map((deckCard) => {
-      const cardDetails = cardData.find((card) => card.name === deckCard.name);
-      return { ...deckCard, imageuri: cardDetails.image_uris.normal };
-    });
-
-    res.send(cardInfo);
-  } catch (err) {
-    res.status(500).send("Error querying card database");
-    return;
-  }
-});
 
 app.post("/newDeckList", async (req, res) => {
   const localstore = [];
   let processedDeck = [];
-  const filePath = path.join(__dirname, "samplemox.txt");
+
   try {
-    const data = await fs.readFile(filePath, "utf8");
+    const data = req.body
     localstore.push(data);
     const lines = localstore[0].split("\n");
     const deck = lines
@@ -77,7 +52,7 @@ app.post("/newDeckList", async (req, res) => {
       .filter((entry) => entry !== null);
     processedDeck = deck;
   } catch (err) {
-    res.status(500).send("Error reading file");
+    res.status(500).send("Error reading data");
     return;
   }
 
---
+++ backend/src/configs/regex.config.js
@@ -0,0 +1,5 @@
+const regex = {
+    titleAndQuant: /^(\d+)\s+([^()]+)\s+\([^()]+\)/, //'1 Frodo, Adventurous Hobbit (LTC) 2 *F*\r'
+}
+
+module.exports = regex;
