ca7f9bf01fe97ea3fba6c79e3ae384486f7691f1
Dylan Sung <dys907@gmail.com>
2025-01-17 23:33:24 -0800

    First working version of moxfield deck builder and parser

--- backend/src/controllers/buildDeck/moxfield.js
+++ backend/src/controllers/buildDeck/moxfield.js
@@ -3,16 +3,19 @@
  * Use the Copy for Moxfield button on the decklist page to get the decklist in plain text
  */
 
-// const got = require("got");
-// const { processDeckLines, findCardInfo } = require("../../../helper/buildDeckHelper");
 import got from "got";
 import { processDeckLines, findCardInfo } from "../../helper/buildDeckHelper.js";
 import regex from "../../configs/regex.config.js";
 
-//https://api.scryfall.com/cards/named?fuzzy=aust+com
-const scryfallApi = new URL(`https://api.scryfall.com/cards/named`)
-const scryfallParams = scryfallApi.searchParams;
-// scryfallApi.searchParams.append("exact", "Sam, Loyal Attendant")
+const scryfallApi = `https://api.scryfall.com/cards/named`
+const scryfallOptions = {
+    headers: {
+        "User-Agent": "NativeMagicProxy/1.0",
+        "Accept": "application/json"
+      }
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 
 const moxfield = async (req, res) => {
@@ -28,31 +31,25 @@ const moxfield = async (req, res) => {
     //api call to get card data from scryfall api
     try {
 
-        const cardDetailsPromises = processedDeck.map(async (deckCard) => {
-            scryfallParams.set("exact", deckCard.name);
-            const cardResponse = await got.get(scryfallApi, {	timeout: {
-                request: 100
-            }});
-            const cardResponseBody = cardResponse.body;
-            console.log(cardResponseBody);
-            const cardData = JSON.parse(cardResponseBody);
-            return { ...deckCard, imageuri: cardData.image_uris.normal };
-            // return scryfallApi
+        const cardDetailsPromises = processedDeck.map(async (deckCard, index) => {
+            //honouring 10 calls or  less a second
+            await delay(100 * index)
+
+            const scryfallUrl = new URL(scryfallApi);
+            scryfallUrl.searchParams.append("exact", deckCard.name);
+             const cardResponse = await got(scryfallUrl, scryfallOptions).json();
+
+            return { ...deckCard, imageuri: cardResponse.image_uris.normal };
           });
       
           const cardDetails = await Promise.all(cardDetailsPromises);
-        // const cardResponse = await got.get('https://api.scryfall.com/cards/named' + `?exact=${processedDeck[0].name}`);
-        // const cardResponse = await got.get(scryfallApi);
-        console.log(cardDetails);
-        // const cardData = JSON.parse(cardResponse.body);
           res.send(cardDetails);
 
     } catch (err) {
         res.status(500).send("Error querying card database");
+        console.log(err);
         return;
     }
-    console.log(scryfallApi);
-    res.send("send")
     
 }
 
